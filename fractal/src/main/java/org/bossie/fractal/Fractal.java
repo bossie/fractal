5ed43f721e26bec42c19b28e32fc43b53eb09ad3
Jan Van den bosch <jan.van.den.bosch@devoteam.be>
2012-06-29 15:32:25 +0200

    initial graphical version -> dump image to file

--- fractal/src/main/java/org/bossie/fractal/Fractal.java
+++ fractal/src/main/java/org/bossie/fractal/Fractal.java
@@ -1,13 +1,16 @@
 package org.bossie.fractal;
 
+import java.awt.image.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 public class Fractal {
-	public static void main(String[] args) {
-		System.out.println(mandelbrot(50, 160));
+	public static void main(String[] args) throws Exception {
+		RenderedImage image = mandelbrot(600, 800);
+		ImageIO.write(image, "png", new File("/tmp/mandelbrot.png"));
 	}
 
-	private static String mandelbrot(int rows, int cols) {
-		StringBuilder sb = new StringBuilder();
-
+	private static RenderedImage mandelbrot(int rows, int cols) {
 		final double xmin = -2.0;
 		final double xmax = 1.0;
 		final double xstep = (xmax - xmin) / cols;
@@ -16,17 +19,18 @@ public class Fractal {
 		final double ymax = 1;
 		final double ystep = (ymax - ymin) / rows;
 
-		final int maxIt = 255;
+		final int maxIt = 0xFF;
 
-		for (double y = ymax; y >= ymin; y -= ystep) {
-			for (double x = xmin; x <= xmax; x += xstep) {
-				sb.append(iterations(x, y, maxIt) >= maxIt ? '#' : ' ');
+		BufferedImage image = new BufferedImage(cols, rows, BufferedImage.TYPE_INT_RGB);
+
+		for (int row = 0; row < rows; ++row) {
+			for (int col = 0; col < cols; ++col) {
+				int it = iterations(col * xstep + xmin, (rows - row) * ystep + ymin, maxIt);
+				image.setRGB(col, row, it << 16 | it << 8 | it);
 			}
-
-			sb.append('\n');
 		}
 
-		return sb.toString();
+		return image;
 	}
 
 	private static int iterations(double x0, double y0, int max) {
@@ -42,7 +46,7 @@ public class Fractal {
 
 			x = tx;
 			y = ty;
-		} while (tx * tx + ty * ty <= 4.0 && i++ < max);
+		} while (tx * tx + ty * ty <= 4.0 && ++i < max);
 
 		return i;
 	}
