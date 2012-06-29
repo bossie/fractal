589b048c417d2ce7bf43db4cc5202487c01bff9a
Jan Van den bosch <jan.van.den.bosch@devoteam.be>
2012-06-29 14:28:11 +0200

    limited text version for testing purposes

--- fractal/src/main/java/org/bossie/fractal/Fractal.java
+++ fractal/src/main/java/org/bossie/fractal/Fractal.java
@@ -2,6 +2,48 @@ package org.bossie.fractal;
 
 public class Fractal {
 	public static void main(String[] args) {
-		System.out.println("Hello Fractal!");
+		System.out.println(mandelbrot(50, 160));
+	}
+
+	private static String mandelbrot(int rows, int cols) {
+		StringBuilder sb = new StringBuilder();
+
+		final double xmin = -2.0;
+		final double xmax = 1.0;
+		final double xstep = (xmax - xmin) / cols;
+
+		final double ymin = -1;
+		final double ymax = 1;
+		final double ystep = (ymax - ymin) / rows;
+
+		final int maxIt = 255;
+
+		for (double y = ymax; y >= ymin; y -= ystep) {
+			for (double x = xmin; x <= xmax; x += xstep) {
+				sb.append(iterations(x, y, maxIt) >= maxIt ? '#' : ' ');
+			}
+
+			sb.append('\n');
+		}
+
+		return sb.toString();
+	}
+
+	private static int iterations(double x0, double y0, int max) {
+		double x = 0;
+		double y = 0;
+
+		int i = 0;
+		double tx, ty;
+
+		do {
+			tx = x * x - y * y + x0;
+			ty = 2 * x * y + y0;
+
+			x = tx;
+			y = ty;
+		} while (tx * tx + ty * ty <= 4.0 && i++ < max);
+
+		return i;
 	}
 }
